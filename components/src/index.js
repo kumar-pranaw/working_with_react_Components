9f24a514fbfa6af3ee528ed6919e0348051d9fb4
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2019-01-23 21:08:42 +0530

    Separating componenets
    Separating commentDetail components to different component

--- components/src/index.js
+++ components/src/index.js
@@ -1,38 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-
+import CommentDetails from './commentDetail'
 const App = () => {
     return (
         <div className="ui conatiner comments">
-        <div className="comment">
-        <a href="/" className = "avatar">
-         <img src={faker.image.avatar()} alt="avatar" />
-        </a>
-        <div className="content">
-            <a href="/" className="author">
-            Sam
-            </a>
-            <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-            </div>
-            <div className="text">Nice blog post!</div>
-        </div>
-        </div>
-        <div className="comment">
-        <a href="/" className = "avatar">
-         <img src={faker.image.avatar()} alt="avatar" />
-        </a>
-        <div className="content">
-            <a href="/" className="author">
-            Sam
-            </a>
-            <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-            </div>
-            <div className="text">Nice blog post!</div>
-        </div>
-        </div>
+            <CommentDetails author="Sam" />
+            <CommentDetails author="Alex"/>
+            <CommentDetails author="Jane"/>
         </div>
     );
 };
