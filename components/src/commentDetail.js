36a05aa9a813d8b272806dccda7c95ab01b6a416
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2019-01-23 21:28:10 +0530

    Extraction of component with props

--- components/src/commentDetail.js
+++ components/src/commentDetail.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import faker from 'faker';
+
 
 const CommentDetail = (props) => {
     console.log(props);
     return (
         <div className="comment">
         <a href="/" className = "avatar">
-         <img src={faker.image.avatar()} alt="avatar" />
+         <img src={props.userImg} alt="avatar" />
         </a>
         <div className="content">
             <a href="/" className="author">
                 {props.author}
             </a>
             <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
+            <span className="date">{props.timeAgo}</span>
             </div>
-            <div className="text">Nice blog post!</div>
+            <div className="text">{props.commentText}</div>
         </div>
       </div>
     );
--- components/src/index.js
+++ components/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetails from './commentDetail'
+import faker from 'faker';
+
 const App = () => {
+    const comment1 = 'Looks like a nice comment'; 
+    const comment2 = 'Hi dear nice pic though'; 
+    const comment3 = 'Having fun!!'; 
     return (
         <div className="ui conatiner comments">
-            <CommentDetails author="Sam" />
-            <CommentDetails author="Alex"/>
-            <CommentDetails author="Jane"/>
+            <CommentDetails author="Sam"  
+            timeAgo = "Today at 4:45PM" 
+            commentText={comment1} userImg={faker.image.avatar()} />
+
+            <CommentDetails author="Alex"
+             timeAgo = "Today at 7:15AM" 
+             commentText={comment2} userImg={faker.image.avatar()}/>
+
+            <CommentDetails author="Jane"
+             timeAgo = "Today at 2:00PM"
+              commentText={comment3} userImg={faker.image.avatar()}/>
         </div>
     );
 };
